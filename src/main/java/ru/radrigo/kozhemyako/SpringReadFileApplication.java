83cc89e9d07c06308e71bfd4dc742064b18013a5
andreykozhemyako <andrey_radrigo@mail.ru>
2018-11-16 20:38:47 +0400

    Создание проекта

--- src/main/java/ru/radrigo/kozhemyako/SpringReadFileApplication.java
+++ src/main/java/ru/radrigo/kozhemyako/SpringReadFileApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringReadFileApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(SpringReadFileApplication.class, args);
     }
